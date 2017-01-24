e22c09ea61104d80137f60cb937e1faafa6ca805
Anu Salminen <aesalmin@gmail.com>
2017-01-24 03:15:19 +0200

    tehtävä 3

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :beers
   resources :breweries
+  resources :ratings
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
